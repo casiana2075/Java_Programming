c14bf0d5d555774c1e75904501b34ff14662de53
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-04-09 19:46:30 +0300

    lab5

--- lab5/Bonus/src/Main.java
+++ lab5/Bonus/src/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
 
@@ -19,60 +17,99 @@ public class Main {
         app.commandHandler();
 
 
-        //excel
-        String excelFilePath = "E:\\Facultate\\an2\\sem2\\Java\\lab5\\Bonus5\\Repository\\Abilities.xlsx";
+        try {
+            // Create a random Excel
+            Workbook workbook = new XSSFWorkbook();
+            Sheet sheet = workbook.createSheet("Employees");
 
-        try (FileInputStream inputStream = new FileInputStream(excelFilePath);
-             Workbook workbook = new XSSFWorkbook(inputStream)) {
+            //list of amilities
+            List<String> abilities = Arrays.asList("secretary", "manager", "tester", "graphic designer", "programming");
 
-            // read from Excel
-            Sheet sheet = workbook.getSheetAt(0);
-            Map<String, Set<String>> employeeAbilitiesMap = new HashMap<>();
+            //header
+            Row headerRow = sheet.createRow(0);
+            headerRow.createCell(0).setCellValue("Name");
+            headerRow.createCell(1).setCellValue("Abilities");
 
-            // skip the header row
-            Iterator<Row> iterator = sheet.iterator();
-            if (iterator.hasNext()) {
-                iterator.next();
+            //fill the sheet with data
+            Random rand = new Random();
+            for (int i = 1; i <= 100; i++) { // 100 rows
+                Row row = sheet.createRow(i);
+                row.createCell(0).setCellValue("employee" + i);
+
+                // random number  of abilityes
+                int numAbilities = rand.nextInt(abilities.size()) + 1;
+                List<String> employeeAbilities = new ArrayList<>();
+                for (int j = 0; j < numAbilities; j++) {
+                    // add a random ability
+                    String ability;
+                    do {
+                        ability = abilities.get(rand.nextInt(abilities.size()));
+                    } while (employeeAbilities.contains(ability));
+                    employeeAbilities.add(ability);
+                }
+
+                // convert to string and set it as the cell value
+                row.createCell(1).setCellValue(String.join(", ", employeeAbilities));
             }
 
-            // process data
-            while (iterator.hasNext()) {
-                Row row = iterator.next();
-                Cell nameCell = row.getCell(0);
-                Cell abilityCell = row.getCell(1);
+            // write the output to a file
+            String excelFilePath = "random_employees.xlsx";
+            try (FileOutputStream fileOut = new FileOutputStream(excelFilePath)) {
+                workbook.write(fileOut);
+            }
 
-                if (nameCell != null && abilityCell != null) {
-                    String name = nameCell.getStringCellValue();
-                    String abilities = abilityCell.getStringCellValue();
+            workbook.close();
 
-                    // split abilities and add them to sets
-                    String[] abilityArray = abilities.split(",");
-                    for (String ability : abilityArray) {
-                        String trimmedAbility = ability.trim();
+            // read from excel
+            try (FileInputStream inputStream = new FileInputStream(excelFilePath);
+                 Workbook workbookRead = new XSSFWorkbook(inputStream)) {
+
+                Sheet sheetRead = workbookRead.getSheetAt(0);
+                Map<String, Set<String>> employeeAbilitiesMap = new HashMap<>();
+
+                // skip the header row
+                Iterator<Row> iterator = sheetRead.iterator();
+                if (iterator.hasNext()) {
+                    iterator.next();
+                }
+
+                // process data
+                while (iterator.hasNext()) {
+                    Row row = iterator.next();
+                    Cell nameCell = row.getCell(0);
+                    Cell abilityCell = row.getCell(1);
+
+                    if (nameCell != null && abilityCell != null) {
+                        String name = nameCell.getStringCellValue();
+                        String ability = abilityCell.getStringCellValue();
+
+                        // add abilities to sets
                         if (!employeeAbilitiesMap.containsKey(name)) {
                             employeeAbilitiesMap.put(name, new HashSet<>());
                         }
-                        employeeAbilitiesMap.get(name).add(trimmedAbility);
+                        employeeAbilitiesMap.get(name).add(ability);
                     }
                 }
-            }
 
-            //grouping
-            Grouping grouping = new Grouping();
-            List<List<String>> maximalGroups = grouping.findMaximalGroups(employeeAbilitiesMap);
+                // grouping
+                Grouping grouping = new Grouping();
+                List<List<String>> maximalGroups = grouping.findMaximalGroups(employeeAbilitiesMap);
 
-            // maximal groups
-            System.out.println("Maximal Groups:");
-            for (List<String> group : maximalGroups) {
-                // employees abilities
-                Set<String> groupAbilities = new HashSet<>();
-                for (String employee : group) {
-                    groupAbilities.addAll(employeeAbilitiesMap.get(employee));
+                // maximal groups
+                System.out.println("Maximal Groups:");
+                for (List<String> group : maximalGroups) {
+                    // employees abilities
+                    Set<String> groupAbilities = new HashSet<>();
+                    for (String employee : group) {
+                        groupAbilities.addAll(employeeAbilitiesMap.get(employee));
+                    }
+
+                    System.out.println("\nAbilities: " + groupAbilities + "\nEmployees list: " + group +"\n");
                 }
 
-                System.out.println("\nAbilities: " + groupAbilities + "\nEmployees list: " + group +"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
